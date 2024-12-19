498e878d872781f362a5763174e6723d2151fd59
jrrodrigo42 <rjr89000@gmail.com>
2024-12-19 12:13:19 -0300

    feat: review

--- src/test/java/br/com/rjrsolucoes/kafkagzipconsumer/KafkagzipconsumerApplicationTests.java
+++ src/test/java/br/com/rjrsolucoes/kafkagzipconsumer/KafkagzipconsumerApplicationTests.java
@@ -1,33 +1,13 @@
 package br.com.rjrsolucoes.kafkagzipconsumer;
 
-import br.com.rjrsolucoes.kafkagzipconsumer.model.ArquivoGerado;
-import br.com.rjrsolucoes.kafkagzipconsumer.repository.ArquivoGeradoRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.mockito.Mockito.*;
-
 @SpringBootTest
-public class KafkagzipconsumerApplicationTests {
+class KafkagzipconsumerApplicationTests {
 
-    @Mock
-    private ArquivoGeradoRepository repository;
+	@Test
+	void contextLoads() {
+	}
 
-    @InjectMocks
-    private KafkagzipconsumerApplication application;
-
-    @Test
-    void testSalvarArquivoNoBanco() {
-        // Cenário
-        ArquivoGerado arquivo = new ArquivoGerado("mensagem de teste", "test-file.gzip");
-
-        // Ação
-        when(repository.save(arquivo)).thenReturn(arquivo);
-        repository.save(arquivo);
-
-        // Verificação
-        verify(repository, times(1)).save(arquivo);
-    }
 }
